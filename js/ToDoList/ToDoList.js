902d58c67e186f9d7957777269a161cda8fdf752
dagaril <dmagaril@ucsd.edu>
2021-02-09 23:20:14 -0800

    Renamed files, added folders

---
+++ js/ToDoList/ToDoList.js
@@ -0,0 +1,31 @@
+class ToDoList {
+    /**
+     * initialze ToDoList
+     * @
+     */
+    constructor(listHTML) {
+      this.taskList = [];
+
+    }
+
+    function addTask(name, totalSession){
+      let task = Task(name, totalSession);
+
+    }
+
+    function addTaskToDom(){}
+    // function function1() {
+    //   var ul = document.getElementById("list");
+    //   var li = document.createElement("li");
+    //   li.appendChild(document.createTextNode("Four"));
+    //   li.setAttribute("id", "element4"); // added line
+    //   ul.appendChild(li);
+    //   alert(li.id);
+    // }
+    function getCurrentTask(){}
+
+
+//    function deleteTask(){}
+}
+
+export { ToDoList };
