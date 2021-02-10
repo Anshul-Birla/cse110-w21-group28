43a54bc1d233e69387305268513166619a8e38a5
Kewen Zhao <Kevin@Kewens-MacBook-Pro.local>
2021-02-09 22:50:24 -0800

    Update

--- js/task.js
+++ js/task.js
@@ -1,7 +1,30 @@
+/*
+ *
+ *
+ *
+ */
 class Task {
-    constructor() {
 
+    /**
+     * initialize a Task
+     * @param {String}
+     * @param {Number}
+     */
+    constructor(id, name, totalSession) {
+      this.id = id;
+      this.name = name;
+      this.totalSession = totalSession;
+      this.currentSessionNum = 0;
+      this.checked = False;
     }
+
+    function incrementSession(){ }
+
+    function checkOffTask(){
+      this.checked = True;
+    }
+
+
 }
 
 export { Task };
