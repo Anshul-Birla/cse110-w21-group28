2d19053039c4af2e02e4aa22df41838d9f45d78e
dagaril <dmagaril@ucsd.edu>
2021-02-10 16:58:32 -0800

    Pseudocode changed to appease linter

--- js/ToDoList/Task.js
+++ js/ToDoList/Task.js
@@ -1,30 +1,26 @@
-/*
- *
- *
- *
- */
 class Task {
+  // constructor, incrementSession, checkOff
 
-    /**
-     * initialize a Task
-     * @param {String}
-     * @param {Number}
-     */
-    constructor(id, name, totalSession) {
-      this.id = id;
-      this.name = name;
-      this.totalSession = totalSession;
-      this.currentSessionNum = 0;
-      this.checked = False;
-    }
+  /**
+  * initialize a Task
+  * @param {String}
+  * @param {Number}
+  */
+  constructor(id, name, totalSessions) {
+    this.id = id;
+    this.name = name;
+    this.totalSessions = totalSessions;
+    this.currentSessionNum = 0;
+    this.checked = false;
+  }
 
-    function incrementSession(){ }
+  incrementSession() {
+    this.currentSessionNum += 1;
+  }
 
-    function checkOffTask(){
-      this.checked = True;
-    }
-
-
+  checkOffTask() {
+    this.checked = true;
+  }
 }
 
 export { Task };
--- js/ToDoList/ToDoList.js
+++ js/ToDoList/ToDoList.js
@@ -1,31 +1,38 @@
+import { Task } from './Task.js';
+
 class ToDoList {
-    /**
-     * initialze ToDoList
-     * @
-     */
-    constructor(listHTML) {
-      this.taskList = [];
+  // constructor, add task (from input fields), add task to dom, get curr task
+  /**
+   * initialze ToDoList
+   * @param {HTMLElement} listHTML
+   */
+  constructor(listHTML) {
+    this.listParent = listHTML;
+    this.taskList = [];
+  }
 
-    }
+  addTask(name, totalSession) {
+    const task = new Task(name, totalSession);
+    this.listParent.appendChild(task);
+  }
 
-    function addTask(name, totalSession){
-      let task = Task(name, totalSession);
+  addTaskToDom() {
+    this.listHTML.append(null);
+  }
 
-    }
+  // function function1() {
+  //   var ul = document.getElementById("list");
+  //   var li = document.createElement("li");
+  //   li.appendChild(document.createTextNode("Four"));
+  //   li.setAttribute("id", "element4"); // added line
+  //   ul.appendChild(li);
+  //   alert(li.id);
+  // }
+  getCurrentTask() {
+    this.addTask(null, null);
+  }
 
-    function addTaskToDom(){}
-    // function function1() {
-    //   var ul = document.getElementById("list");
-    //   var li = document.createElement("li");
-    //   li.appendChild(document.createTextNode("Four"));
-    //   li.setAttribute("id", "element4"); // added line
-    //   ul.appendChild(li);
-    //   alert(li.id);
-    // }
-    function getCurrentTask(){}
-
-
-//    function deleteTask(){}
+  //    function deleteTask(){}
 }
 
 export { ToDoList };
