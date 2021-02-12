97f74a5fd84710fdf16ceffe21d40b78a5c04ab5
dagaril <dmagaril@ucsd.edu>
2021-02-11 21:00:20 -0800

    initial run through add html children to task

--- js/ToDoList/Task.js
+++ js/ToDoList/Task.js
@@ -13,11 +13,64 @@ class Task extends HTMLElement {
   */
   constructor(id, name, totalSessions) {
     super();
+
     this.id = id;
     this.name = name;
     this.totalSessions = totalSessions;
     this.currentSessionNum = 0;
     this.checked = false;
+    this.setAttribute('class', 'notCheckedOffTask');
+
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.setAttribute('id', `checkbox-${this.id}`);
+    checkBox.addEventListener('click', this.checkOffTask());
+    this.appendChild(checkBox);
+
+    const text = document.createElement('p');
+    text.setAttribute('id', `text-${this.id}`);
+    this.appendChild(text);
+    this.updateText();
+
+    const pomoSessions = document.createElement('p');
+    pomoSessions.setAttribute('id', `pomoSessions-${this.id}`);
+    this.appendChild(pomoSessions);
+    this.updatePomoSessions();
+  }
+
+  updateText() {
+    this.children[1].textContent = this.name;
+  }
+
+  updatePomoSessions() {
+    this.children[2].textContent = `[${this.currentSessionNum}/\
+      ${this.totalSessions}]`;
+  }
+
+  /**
+   * Increment this task's current session number. Checks off task if all sessions completed.
+   * @throws {RangeError} - Incrementing a completed tasks' session number
+   * (all sessions have been completed or user has checked off task manually)
+   */
+  incrementSession() {
+    if (this.checked) {
+      throw (new RangeError());
+    }
+
+    this.currentSessionNum += 1;
+    this.updatePomoSessions();
+
+    if (this.currentSessionNum === this.totalSessions) {
+      this.checkOffTask();
+    }
+  }
+
+  /**
+   * Mark task as completed
+   */
+  checkOffTask() {
+    this.checked = true;
+    this.setAttribute('class', 'checkedOffTask');
   }
 
   // GETTERS AND SETTERS IF WE NEED THEM LATER. Currently, setter causes infinite recursion.
@@ -50,31 +103,6 @@ class Task extends HTMLElement {
    * @ignore
    * @type {number} */
   // set currentSessionNum(num) { this.currentSessionNum = num; }
-
-  /**
-   * Increment this task's current session number. Checks off task if all sessions completed.
-   * @throws {RangeError} - Incrementing a completed tasks' session number
-   * (all sessions have been completed or user has checked off task manually)
-   */
-  incrementSession() {
-    if (this.checked) {
-      throw (new RangeError());
-    }
-
-    this.currentSessionNum += 1;
-    this.textContent = this.currentSessionNum;
-
-    if (this.currentSessionNum === this.totalSessions) {
-      this.checkOffTask();
-    }
-  }
-
-  /**
-   * Mark task as completed
-   */
-  checkOffTask() {
-    this.checked = true;
-  }
 }
 
 export { Task };
