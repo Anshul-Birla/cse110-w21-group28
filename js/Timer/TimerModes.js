13ad06f71bf58b4e563fdf3efbc08b8fcc3cab2c
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2021-02-15 20:42:19 -0800

    v0.1.0 Pomodoro Timer (#29)
    Adds a Basic Timer that autorotates between sessions

---
+++ js/Timer/TimerModes.js
@@ -0,0 +1,17 @@
+// these are all variables for the Timer. Changing these will change them universally
+
+const workMode = {
+  name: 'Working Time',
+  duration: 25,
+};
+const shortBreakMode = {
+  name: 'Short Break',
+  duration: 5,
+};
+
+const longBreakMode = {
+  name: 'Long Break',
+  duration: 15,
+};
+
+export { workMode, shortBreakMode, longBreakMode };
