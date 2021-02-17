c0ad60a6bd75258715a5e441c6d6773d8a8710f5
Ravi <rtapia@ucsd.edu>
2021-02-16 20:11:18 -0800

    edited start

--- js/changeDom.js
+++ js/changeDom.js
@@ -1,6 +1,6 @@
 import { increment } from './function';
-import { Timer } from './timer.js'
- 
+import { Timer } from './timer.js';
+
 const button = document.getElementById('incrementButton');
 const output = document.getElementById('outputIncrement');
 button.addEventListener('click', () => {
@@ -12,18 +12,17 @@ button.addEventListener('click', () => {
  * Start Button
  * When Clicked, start timer. Switches Text from Start --> End Session (vice versa).
  */
-let TimerObj = new Timer(document.getElementById('timeDisplay'),document.getElementById('modeDisplay'));
+const TimerObj = new Timer(document.getElementById('timeDisplay'), document.getElementById('modeDisplay'));
 // this starts the Timer on click & switches text from 'Start' <--> ' End Session '
-document.getElementById("startButton").addEventListener('click',function(){
+document.getElementById('startButton').addEventListener('click', () => {
   // if button is a start button:
-   if(document.getElementById("startButton").innerHTML == "Start"){
-     startBtnElement.innerHTML = "End Session"; 
-     startBtnElement.style.backgroundColor = "Grey";
-     TimerObj.startTimer();
-    }else{
-      //switches from end button to start button
-      startBtnElement.innerText = "Start";  
-      startBtnElement.style.backgroundColor = "Green";
-
-   }
+  if (document.getElementById('startButton').innerHTML == 'Start') {
+    document.getElementById('startButton').innerHTML = 'End Session';
+    document.getElementById('startButton').style.backgroundColor = 'Grey';
+    TimerObj.startTimer();
+  } else {
+    // switches from end button to start button
+    document.getElementById('startButton').innerText = 'Start';
+    document.getElementById('startButton').style.backgroundColor = 'Green';
+  }
 });
