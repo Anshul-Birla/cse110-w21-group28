c6a634fe58d064ccbe5fc015fd2cd5fca68d3a41
kyeling <kyeling@users.noreply.github.com>
2021-02-16 20:00:30 -0800

    addressed comments on centering timer and single quotes in App.js, added comments in index.html, need to add classes in html/css

--- js/App.js
+++ js/App.js
@@ -1,8 +1,8 @@
 import { Timer } from './Timer/Timer.js';
 
 const timeDisplay = document.getElementById('timeDisplay');
-const modeDisplay = document.getElementById("modeDisplay");
-const startTimerButton = document.getElementById("startTimer");
+const modeDisplay = document.getElementById('modeDisplay');
+const startTimerButton = document.getElementById('startTimer');
 
 const TimerObj = new Timer(timeDisplay, modeDisplay);
 
