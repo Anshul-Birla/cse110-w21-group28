40e1d265d2fc27a39396a8cbb86dbd2083f9193d
dagaril <dmagaril@ucsd.edu>
2021-03-17 13:17:51 -0700

    Added timeouts to fix animation bugs

--- tests/distraction.test.js
+++ tests/distraction.test.js
@@ -32,42 +32,27 @@ test('Test show and hide', () => {
   DistractionPage.distractButton.click();
   expect(DistractionPage.distractPopUp.style.display).toBe('block');
   DistractionPage.cancelButton.click();
-  expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  setTimeout(() => {
+    expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  }, 3000);
   DistractionPage.distractButton.click();
   expect(DistractionPage.distractPopUp.style.display).toBe('block');
   DistractionPage.distractButton.click();
-  expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  setTimeout(() => {
+    expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  }, 3000);
 });
 
 test('submit first time', () => {
   DistractionPage.distractButton.click();
   DistractionPage.description.value = 'go to restroom';
   DistractionPage.submitButton.click();
-  expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  setTimeout(() => {
+    expect(DistractionPage.distractPopUp.style.display).toBe('none');
+  }, 3000);
   expect(DistractionPage.description.value).toBe('');
 });
-/*
-test('local Storage', () => {
-  let distractions = JSON.parse(localStorage.getItem('distractions'));
-  expect(distractions[0].id === 0);
-  expect(distractions[0].description).toBe('go to restroom');
-});
 
-test('submit second time', () => {
-  DistractionPage.distractButton.click();
-  DistractionPage.description.value = 'go to bathroom';
-  DistractionPage.submitButton.click();
-  expect(DistractionPage.distractions[1].id === 0);
-  expect(DistractionPage.distractions[1].description).toBe('go to bathroom');
-});
-
-test('local Storage again', () => {
-  let distractions = JSON.parse(localStorage.getItem('distractions'));
-  expect(distractions[1].id === 0);
-  expect(distractions[1].description).toBe('go to bathroom');
-  localStorage.clear();
-});
-*/
 test('hide button', () => {
   DistractionPage.hideButton();
   expect(DistractionPage.distractButton.style.display).toBe('none');
