08f0e7e34db9ec94cda0a56277c7b20fab6b380f
Ryan <ryan.h.nishimoto@gmail.com>
2021-03-17 00:30:01 -0700

    add assets

---
+++ js/Sounds.js
@@ -0,0 +1,67 @@
+const horn = document.getElementById('alert-sound');
+
+const workSoundSelector = document.getElementById('workSoundSelector');
+const breakSoundSelector = document.getElementById('breakSoundSelector');
+const airHorn = './assets/audio/air-horn.mp3';
+const celebration = './assets/audio/celebration.mp3';
+const error = './assets/audio/error.mp3';
+const noot = './assets/audio/noot.mp3';
+const ping = './assets/audio/ping.mp3';
+const siren = './assets/audio/siren.mp3';
+const muteSwitch = document.getElementById('muteSwitch');
+
+
+var workModeSoundPath = airHorn;
+var breakModeSoundPath = celebration;
+export function workModeSound(){
+    if (muteSwitch.checked == true){
+        return;
+    }
+    horn.setAttribute('src', workModeSoundPath);
+    horn.play();
+}
+
+export function breakModeSound(){
+    if(muteSwitch.checked ==true){
+        return;
+    }
+    horn.setAttribute('src', breakModeSoundPath);
+    horn.play();
+}
+
+function changeSound(e){
+    let path;
+    switch(e.target.value){
+        case 'horn':
+            path = airHorn;
+            break;
+        case 'celebration':
+            path = celebration;
+            break;
+        case 'error':
+            path = error;
+            break;
+        case 'noot':
+            path = noot;
+            break;
+        case 'ping':
+            path = ping;
+            break;
+        case 'siren':
+            path = siren;
+            break;
+    }
+    if (muteSwitch.checked == false){
+        horn.setAttribute('src', path);
+        horn.play();
+    }
+    if(e.target.id === 'workSoundSelector'){
+        workModeSoundPath = path;
+    }
+    else {
+        breakModeSoundPath = path;
+    }
+}
+
+workSoundSelector.addEventListener('change', changeSound);
+breakSoundSelector.addEventListener('change', changeSound);
