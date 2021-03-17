88b443ecff249d6e1ad5a92cef9485815a23425c
amakhnia <amakhnia@ucsd.edu>
2021-03-17 14:55:51 -0700

    fixed lint error

--- js/FAQ/FAQ.js
+++ js/FAQ/FAQ.js
@@ -19,7 +19,7 @@ span.onclick = function () {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
 };
