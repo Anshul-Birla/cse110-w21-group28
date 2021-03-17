637405451116636bb6baf6b963be381c99737ac4
Anshul-Birla <anshulbirla@gmail.com>
2021-03-17 01:03:49 -0700

    added no script

--- js/App.js
+++ js/App.js
@@ -14,6 +14,7 @@ const deleteAllButton = document.getElementById('delete-all-button');
 const TDLDom = new TodoListDom(todoTable, addTodoForm, addTodoButton, deleteAllButton);
 const TimerObj = new Timer(startTimerButton, timeDisplay, modeDisplay);
 
+
 TimerObj.addEventListener('timer-complete', (e) => {
   if (e.detail.sessionName === workMode.name) {
     TDLDom.onSessionComplete();
@@ -58,3 +59,5 @@ window.addEventListener('click', (e) => {
     }
   }
 });
+
+
