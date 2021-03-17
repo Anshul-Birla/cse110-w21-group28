2fd77eea0460d84b17e7cbc3a723c9876a08009c
kewen <k4zhao@ucsd.edu>
2021-03-17 15:43:25 -0700

    fix current task bug

--- js/ToDoList/TodoListDom.js
+++ js/ToDoList/TodoListDom.js
@@ -171,9 +171,10 @@ class TodoListDom {
     checked off, not the new current task. Only works if the user
     can only check off the current task
     */
-    this.currentTask.onDelete();
-    this.displayTask(this.currentTask);
-    this.todoList.addTaskToEnd(this.currentTask);
+    let temp = this.currentTask;
+    temp.onDelete();
+    this.displayTask(temp);
+    this.todoList.addTaskToEnd(temp);
   }
 
   /**
