49221b667e3a0b4f68f58641870cf3c157d51a41
kewen <k4zhao@ucsd.edu>
2021-03-18 14:19:44 -0700

    documentation

--- js/ToDoList/TodoListDomVariables.js
+++ js/ToDoList/TodoListDomVariables.js
@@ -4,6 +4,12 @@ const HTMLAttributes = {
   taskNameId: 'task-name',
   taskPomoSessions: 'task-length',
 };
+
+/**
+ * The object that stores the corresponding index of each element.
+ * Used in the array of JSON.parse(localStorage.getItem('tasks'))
+ * @type {Object} TaskStorage
+ */
 const TaskStorage = {
   idIndex: 0,
   nameIndex: 1,
