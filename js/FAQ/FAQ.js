637bf9498511e8bae760411214d00a940ea5acba
amakhnia <amakhnia@ucsd.edu>
2021-03-17 19:41:00 -0700

    edited js to match linter, edited variables and colors to match, as well as edited background to be transparent

--- js/FAQ/FAQ.js
+++ js/FAQ/FAQ.js
@@ -1,5 +1,5 @@
 // Get the modal
-const modal = document.getElementById('myModal');
+const modalFAQ = document.getElementById('myModal');
 
 // Get the button that opens the modal
 const btn = document.getElementById('faqButton');
@@ -8,18 +8,19 @@ const btn = document.getElementById('faqButton');
 const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+btn.addEventListener('click', () => {
+    modalFAQ.style.display = 'block';
+  });
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+span.addEventListener('click',() =>  {
+  modalFAQ.style.display = 'none';
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.addEventListener('click' , (event) => {
   if (event.target === modal) {
-    modal.style.display = 'none';
+    modalFAQ.style.display = 'none';
   }
-};
+});
