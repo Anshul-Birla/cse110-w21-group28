1f51d21040f775d8735e8d2ea0a2292f6de7eedd
amakhnia <amakhnia@ucsd.edu>
2021-03-17 19:43:57 -0700

    fixed lint

--- js/FAQ/FAQ.js
+++ js/FAQ/FAQ.js
@@ -9,18 +9,17 @@ const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks the button, open the modal
 btn.addEventListener('click', () => {
-    modalFAQ.style.display = 'block';
-  });
-
+  modalFAQ.style.display = 'block';
+});
 
 // When the user clicks on <span> (x), close the modal
-span.addEventListener('click',() =>  {
+span.addEventListener('click', () => {
   modalFAQ.style.display = 'none';
 });
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click' , (event) => {
-  if (event.target === modal) {
+window.addEventListener('click', (event) => {
+  if (event.target === modalFAQ) {
     modalFAQ.style.display = 'none';
   }
 });
